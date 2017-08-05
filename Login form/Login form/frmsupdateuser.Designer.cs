14c0c240ad2dab08264717e3ba36ebf859b8221d
Graxiang <marygarce.puyot@jmc.edu.ph>
2017-08-05 18:42:22 +0800

    User Management

---
+++ Login form/Login form/frmsupdateuser.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Login_form
+{
+    partial class frmsupdateuser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnudpate = new System.Windows.Forms.Button();
+            this.cbUpdateasssecretary = new System.Windows.Forms.CheckBox();
+            this.cbUpdateowner = new System.Windows.Forms.CheckBox();
+            this.cbUpdatemale = new System.Windows.Forms.CheckBox();
+            this.cbUpdatefemale = new System.Windows.Forms.CheckBox();
+            this.txtUpdatecontactno = new System.Windows.Forms.TextBox();
+            this.txtUpdateemail = new System.Windows.Forms.TextBox();
+            this.txtUpdatebday = new System.Windows.Forms.TextBox();
+            this.txtUpdateage = new System.Windows.Forms.TextBox();
+            this.txtUpdateLastname = new System.Windows.Forms.TextBox();
+            this.txtUpdateMiddlename = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtUpdateFirstname = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnudpate
+            // 
+            this.btnudpate.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnudpate.Location = new System.Drawing.Point(508, 211);
+            this.btnudpate.Name = "btnudpate";
+            this.btnudpate.Size = new System.Drawing.Size(75, 23);
+            this.btnudpate.TabIndex = 43;
+            this.btnudpate.Text = "Update";
+            this.btnudpate.UseVisualStyleBackColor = false;
+            // 
+            // cbUpdateasssecretary
+            // 
+            this.cbUpdateasssecretary.AutoSize = true;
+            this.cbUpdateasssecretary.Location = new System.Drawing.Point(477, 21);
+            this.cbUpdateasssecretary.Name = "cbUpdateasssecretary";
+            this.cbUpdateasssecretary.Size = new System.Drawing.Size(116, 17);
+            this.cbUpdateasssecretary.TabIndex = 42;
+            this.cbUpdateasssecretary.Text = "Assistant Secretary";
+            this.cbUpdateasssecretary.UseVisualStyleBackColor = true;
+            // 
+            // cbUpdateowner
+            // 
+            this.cbUpdateowner.AutoSize = true;
+            this.cbUpdateowner.Location = new System.Drawing.Point(398, 21);
+            this.cbUpdateowner.Name = "cbUpdateowner";
+            this.cbUpdateowner.Size = new System.Drawing.Size(57, 17);
+            this.cbUpdateowner.TabIndex = 41;
+            this.cbUpdateowner.Text = "Owner";
+            this.cbUpdateowner.UseVisualStyleBackColor = true;
+            // 
+            // cbUpdatemale
+            // 
+            this.cbUpdatemale.AutoSize = true;
+            this.cbUpdatemale.Location = new System.Drawing.Point(172, 174);
+            this.cbUpdatemale.Name = "cbUpdatemale";
+            this.cbUpdatemale.Size = new System.Drawing.Size(49, 17);
+            this.cbUpdatemale.TabIndex = 40;
+            this.cbUpdatemale.Text = "Male";
+            this.cbUpdatemale.UseVisualStyleBackColor = true;
+            // 
+            // cbUpdatefemale
+            // 
+            this.cbUpdatefemale.AutoSize = true;
+            this.cbUpdatefemale.Location = new System.Drawing.Point(89, 175);
+            this.cbUpdatefemale.Name = "cbUpdatefemale";
+            this.cbUpdatefemale.Size = new System.Drawing.Size(60, 17);
+            this.cbUpdatefemale.TabIndex = 39;
+            this.cbUpdatefemale.Text = "Female";
+            this.cbUpdatefemale.UseVisualStyleBackColor = true;
+            // 
+            // txtUpdatecontactno
+            // 
+            this.txtUpdatecontactno.Location = new System.Drawing.Point(398, 101);
+            this.txtUpdatecontactno.Name = "txtUpdatecontactno";
+            this.txtUpdatecontactno.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdatecontactno.TabIndex = 38;
+            // 
+            // txtUpdateemail
+            // 
+            this.txtUpdateemail.Location = new System.Drawing.Point(398, 59);
+            this.txtUpdateemail.Name = "txtUpdateemail";
+            this.txtUpdateemail.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdateemail.TabIndex = 37;
+            // 
+            // txtUpdatebday
+            // 
+            this.txtUpdatebday.Location = new System.Drawing.Point(89, 213);
+            this.txtUpdatebday.Name = "txtUpdatebday";
+            this.txtUpdatebday.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdatebday.TabIndex = 36;
+            // 
+            // txtUpdateage
+            // 
+            this.txtUpdateage.Location = new System.Drawing.Point(89, 141);
+            this.txtUpdateage.Name = "txtUpdateage";
+            this.txtUpdateage.Size = new System.Drawing.Size(77, 20);
+            this.txtUpdateage.TabIndex = 35;
+            // 
+            // txtUpdateLastname
+            // 
+            this.txtUpdateLastname.Location = new System.Drawing.Point(89, 101);
+            this.txtUpdateLastname.Name = "txtUpdateLastname";
+            this.txtUpdateLastname.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdateLastname.TabIndex = 34;
+            // 
+            // txtUpdateMiddlename
+            // 
+            this.txtUpdateMiddlename.Location = new System.Drawing.Point(89, 66);
+            this.txtUpdateMiddlename.Name = "txtUpdateMiddlename";
+            this.txtUpdateMiddlename.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdateMiddlename.TabIndex = 33;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(246, 119);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 32;
+            // 
+            // txtUpdateFirstname
+            // 
+            this.txtUpdateFirstname.Location = new System.Drawing.Point(89, 19);
+            this.txtUpdateFirstname.Name = "txtUpdateFirstname";
+            this.txtUpdateFirstname.Size = new System.Drawing.Size(185, 20);
+            this.txtUpdateFirstname.TabIndex = 31;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(320, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Contact No";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(320, 66);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Email-add";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(320, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Position";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Birthday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Lastname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Middlename";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Firstname";
+            // 
+            // frmsupdateuser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(606, 256);
+            this.Controls.Add(this.btnudpate);
+            this.Controls.Add(this.cbUpdateasssecretary);
+            this.Controls.Add(this.cbUpdateowner);
+            this.Controls.Add(this.cbUpdatemale);
+            this.Controls.Add(this.cbUpdatefemale);
+            this.Controls.Add(this.txtUpdatecontactno);
+            this.Controls.Add(this.txtUpdateemail);
+            this.Controls.Add(this.txtUpdatebday);
+            this.Controls.Add(this.txtUpdateage);
+            this.Controls.Add(this.txtUpdateLastname);
+            this.Controls.Add(this.txtUpdateMiddlename);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtUpdateFirstname);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmsupdateuser";
+            this.Text = "frmsupdateuser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnudpate;
+        private System.Windows.Forms.CheckBox cbUpdateasssecretary;
+        private System.Windows.Forms.CheckBox cbUpdateowner;
+        private System.Windows.Forms.CheckBox cbUpdatemale;
+        private System.Windows.Forms.CheckBox cbUpdatefemale;
+        private System.Windows.Forms.TextBox txtUpdatecontactno;
+        private System.Windows.Forms.TextBox txtUpdateemail;
+        private System.Windows.Forms.TextBox txtUpdatebday;
+        private System.Windows.Forms.TextBox txtUpdateage;
+        private System.Windows.Forms.TextBox txtUpdateLastname;
+        private System.Windows.Forms.TextBox txtUpdateMiddlename;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtUpdateFirstname;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
